44048e24efd31f533ea997315bd7d60194d8be11
marco <marcoiban@gmail.com>
2015-03-01 21:42:15 +0100

    metodo para averiguar la existencia de un numero

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,33 +1,49 @@
+import java.util.ArrayList;
 
-/**
- * Write a description of class MyHashSet here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyHashSet
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private int[] numbers;
 
-    /**
-     * Constructor for objects of class MyHashSet
-     */
+   
     public MyHashSet()
     {
-        // initialise instance variables
-        x = 0;
+       this.numbers=new int[0];
+       
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * add(int valor): añade el elemento al
+     * conjunto si no estaba. Devuelve verdadero 
+     * en caso de que el elemento no estuviera presente
+     * en el conjunto y falso en caso contrario.
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+   public boolean add(int valor)
+   {
+       boolean exist = false;
+       int index = 0;
+       
+       while(index < numbers.length && !exist)
+       {
+           if(numbers[index] == valor)
+           {
+               exist = true;
+               
+            }
+            index++;
+       }
+       
+       if(exist==false)
+       {
+           int pos=0;
+           int[] newnumbers = new int[numbers.length + 1];
+           while(pos<numbers.length)
+           {
+               newnumbers[pos]=numbers[pos];
+               pos++;
+            }
+           newnumbers [numbers.length]=valor;
+           numbers=newnumbers;
+       }
+       return exist;
+   }
 }
